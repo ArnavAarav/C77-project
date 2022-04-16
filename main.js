39085b6f3a75f4d70e243f323fcda6594c9eaf36
ArnavAarav <91784800+ArnavAarav@users.noreply.github.com>
2022-04-16 11:59:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Console");
+var first_name = "Arnav";
+var last_name = "Harsh";
+console.log(first_name.concat(last_name));
+var a = 10;
+var b = 5;
+
+var add = (a+b);
+console.log(add);
+
+var subtract = (a-b);
+console.log(subtract);
+
+var multiply = (a*b);
+console.log(multiply);
+
+var divide = (a/b);
+console.log(divide);
